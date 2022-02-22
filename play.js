568ab2854b09aa157f49976ca0bd3cce58ae6842
Liam Smith <liam.s.smith@gmail.com>
2022-02-22 21:51:06 +0000

    move connet function to client file

--- play.js
+++ play.js
@@ -1,24 +1,26 @@
-const net = require('net');
+
+const { connect} = require('./client');
+
 
 // establishes a connection with game server
- const connect = function() {
-   const conn = net.createConnection({
-     host: "localhost",
-     port: 50541,
-   });
+//  const connect = function() {
+//    const conn = net.createConnection({
+//      host: "localhost",
+//      port: 50541,
+//    });
 
-    conn.on("data", (data) => {
-      console.log('Server says: ', data);
-    });
+//     conn.on("data", (data) => {
+//       console.log('Server says: ', data);
+//     });
  
-    conn.on("connect", () => {     // code that does something when the connection is first established     
-      console.log('Successfully connected to the game server');
-    });
+//     conn.on("connect", () => {     // code that does something when the connection is first established     
+//       console.log('Successfully connected to the game server');
+//     });
 
- conn.setEncoding('utf8');
+//  conn.setEncoding('utf8');
 
- return conn;
-};
+//  return conn;
+// };
 
 console.log('connecting...');
 
