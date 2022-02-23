37f3a0fd275dea3c1995b655d881ba37d72c8128
Liam Smith <liam.s.smith@gmail.com>
2022-02-23 04:55:00 +0000

    set WASD to move snake

---
+++ input.js
@@ -0,0 +1,34 @@
+// setup interface to handle user input from stdin
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === '\u0077') {
+      conn.write('Move: up');
+    }
+    if (key === '\u0064') {
+      conn.write('Move: right');
+    }
+    if (key === '\u0061') {
+      conn.write('Move: left');
+    }
+    if (key === '\u0073') {
+      conn.write('Move: down');
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {setupInput}
