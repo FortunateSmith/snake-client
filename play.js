bd0c7a39baba6a1bce1e27f9f8271b69f9b9fd0e
Liam Smith <liam.s.smith@gmail.com>
2022-02-23 04:56:08 +0000

    send command strings to server using connection object

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
-
-const { connect} = require('./client');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('connecting...');
-setupInput();
-connect();
+const connection = connect()
+
+setupInput(connection);
+//connect();
